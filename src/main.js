7eaeea9fac7306e42e9ed79af63dcad83ce339ec
Julien Klein <44435573+iLdro@users.noreply.github.com>
2023-06-16 15:34:35 +0200

    seting up .env

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import dotenv from 'dotenv';
+dotenv.config();
 
 createApp(App).use(router).mount("#app");
