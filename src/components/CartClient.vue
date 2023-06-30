f661c548e6b079f72324e8ba74924b9ce1fb8ddb
Edran0111 <82849327+Edran0111@users.noreply.github.com>
2023-06-30 11:49:07 +0200

    Client bon

--- src/components/CartClient.vue
+++ src/components/CartClient.vue
@@ -14,17 +14,16 @@
 
         <template v-for="(prescriptions, index) in prescriptions" :key="index">
           <template v-if="prescriptions.expired === false">
-          
             <tr>
               <td colspan="4">Prescription {{ index + 1 }}</td>
             </tr>
-            <tr v-for="(medicaments, index) in prescriptions.medicaments" :key="index">
+            <tr v-for="(medicaments, subIndex) in prescriptions.medicaments" :key="subIndex">
               <td>{{ medicaments.nom_medicament }}</td>
               <td>{{ medicaments.dosage }}</td>
               <td>{{ medicaments.fréquence }}</td>
               <td>{{ medicaments.duree }}</td>
-              <td><img :src="prescriptions.image_url"></td>
-             
+              
+              <td><a @click="displayOrdo(prescriptions)">Afficher</a></td>
             </tr>
           </template>
         
@@ -37,11 +36,11 @@
 <script>
 import ProductService from "@/services/ProductService.js";
 import axios from "axios";
+
 import { initializeApp } from 'firebase/app';
 import { getStorage } from "firebase/storage";
-//import { ref, getDownloadURL } from 'firebase/storage';
-import { ref} from 'firebase/storage';
 
+import { ref} from 'firebase/storage';
 
 export default {
   props: ["cart"],
@@ -49,31 +48,61 @@ export default {
     return {
       prescriptions: [],
       imageUrl: "",
-      
+      medicament: [],
     }; 
   },
   methods: {
-    convertBSONToPNG(bsonData) {
-    fetch('http://localhost:5000/convert', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ bsonData }),
-    })
-      .then(response => response.blob())
-      .then(blob => {
-        // Create a URL for the blob object
-        const url = URL.createObjectURL(blob);
-
-        // Use the URL to display the PNG file
-        this.imageUrl = url;
-      })
-      .catch(error => {
-        console.log('Error converting BSON to PNG:', error);
+    displayOrdo(prescriptions) {
+      // Find the medications for the selected prescription
+      const medications = prescriptions.medicaments.map(med => {
+        return {
+          nom_medicament: med.nom_medicament,
+          dosage: med.dosage,
+          frequence: med.fréquence,
+          duree: med.duree,
+        };
       });
-  },
-  },
+
+      this.medicament = medications; // Assign the medications to the variable
+      console.log("MEDICAMENTS")
+      console.log(this.medicament)
+      axios.post("http://localhost:5173/ordonnance", 
+        {
+
+          client_id: this.prescriptions[0].user_id,
+          medecin_id: this.prescriptions[0].medecin_id,
+          medicaments: this.medicament,
+        },{responseType: 'blob'})
+       
+       
+        .then(response => {
+          //console.log("REPONSE")
+          //console.log(response.data);
+          //this.imageUrl = response.data._id;
+          //console.log("IMAGE URL")
+          //console.log(this.imageUrl)
+          // const blob = new Blob([response.data], { type: 'image/png' });
+
+          // // Create object URL for the Blob
+          // const imageUrl = URL.createObjectURL(blob);
+
+          // console.log("IMAGE URL")
+          // console.log(imageUrl)
+          
+          // window.open(imageUrl, '_blank');
+          console.log("REPONSE",response.data)
+          const url = window.URL.createObjectURL(response.data);
+          console.log("URL",url)
+          window.open(url, '_blank');
+        },)
+        .catch(error => {
+          console.log(error);
+        });
+        
+      }
+    
+    
+    },
   created() {
     ProductService.getOrdonnance("64958e29428e5f9a03cba8ca")
       .then(response => {
@@ -82,7 +111,10 @@ export default {
         this.prescriptions = response.data;
         
         console.log("ORDONNNANCE")
-        console.log(this.prescriptions);
+        console.log(this.prescriptions)
+        console.log("User ID:", this.prescriptions[0].user_id);
+      console.log("Medecin ID:", this.prescriptions[0].medecin_id);
+      console.log("Medicaments:", this.prescriptions[0].medicaments);
       })
       .catch(error => {
         console.log(error);
@@ -106,37 +138,17 @@ export default {
     const imageRef = ref(storage, '1687781370.jpg');
    
     console.log(imageRef)
-
-    axios.post('http://localhost:5000/user/getImages',  {id:"649c017474ae4369162836bb"})
-        .then((url) => {
-          // Use the URL to access and display the image
-          console.log("YOUPI CA MARCHE")
-          console.log(url);
-          this.imageUrl = url.data;
-          console.log(url.data.image)
-          this.convertBSONToPNG(url.data.image);
-
-          })
-          .catch((error) => {
-            console.log("ici erreur");
-            // Handle any errors that occur
-            console.log(error);
-          });
-              
-            },
+  }
+};
     
-    mounted() {
-      
-    }
+    
+ 
 
 
-};</script>
+</script>
 
 <style scoped>
-.user-info {
-  display: flex;
-  justify-content: center;
-}
+
 table {
   width: 1500px;
 }
@@ -149,48 +161,11 @@ th {
   text-decoration: underline;
   width: 150px;
 }
-.Remove {
-  width: 370px;
-}
-table img {
-  max-width: 50%;
-}
-.bigcontainer {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: rgb(0, 0, 0);
-}
-.filters {
-  border: 1px solid black;
-}
-.products {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  color: rgb(0, 0, 0);
-}
-.card {
-  position: relative;
-  width: 350px;
-  height: 350px;
-  background: rgb(238, 237, 237);
-  border-radius: 50px;
-  background-color: rgba(71, 70, 70, 0.103);
-}
-.card .imgBox {
-  position: relative;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  padding-top: 20px;
-  z-index: 1;
-}
-.card .imgBox img {
-  max-width: 55%;
-}
+
+
+
+
+
 button {
   background-image: linear-gradient(to right, #00a38c, #0054a3);
   opacity: 0.8;
@@ -211,29 +186,7 @@ button:hover {
   color: #fff;
   text-decoration: none;
 }
-.custom-file-upload {
-  display: inline-block;
-  padding: 10px 20px;
-  background-color: #f2f2f2;
-  color: #333;
-  border-radius: 5px;
-  cursor: pointer;
-  font-weight: bold;
-}
 
-.upload-button {
-  padding: 10px 20px;
-  background-color: #4285f4;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-weight: bold;
-  width: 10%;
-}
-.ordo{
-  visibility:hidden;
-}
 
 
 input {
